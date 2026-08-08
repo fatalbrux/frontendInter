46d53b43bf7ccd9d946de7cf664fb012d899e9ac
fatalbrux <henry.quispe@sistemas.edu.bo>
2026-08-08 17:19:56 -0400

    Corrigiendo URL del auth

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -3,10 +3,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { LoginResponse } from '../interfaces/auth';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
